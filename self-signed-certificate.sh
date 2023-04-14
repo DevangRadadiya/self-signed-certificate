ab990cd8a213050e8056fef70055084a2a8a94d0
Devang Radadiya <devang24393@hotmail.co.uk>
2023-04-14 14:07:04 +0100

    Update self-signed-certificate.sh

--- self-signed-certificate.sh
+++ self-signed-certificate.sh
@@ -1,57 +1,60 @@
 #!/bin/bash
 
-echo "Enter the domain name (without https://www. and extensions like .com, .org):"
-read domain_name
+# User input
+read -p "Enter domain name (e.g. example.com): " domain_name
+read -p "Enter subject's Country (C): " country
+read -p "Enter subject's State (ST): " state
+read -p "Enter subject's Locality (L): " locality
+read -p "Enter subject's Organization (O): " organization
+read -p "Enter subject's Organizational Unit (OU): " organizational_unit
+read -p "Enter subject's Common Name (CN): " common_name
+read -p "Enter subject's Email Address: " email_address
+read -p "Enter certificate expire days: " expire_days
+read -sp "Enter passphrase: " passphrase
 
-# Remove the * character from the beginning of the domain name, if any
-if [[ $domain_name == *\** ]]; then
-  domain_name=${domain_name#*\*}
-  folder_name="_${domain_name}"
+# Create issuer certificate
+openssl req -new -x509 -nodes -keyout issuer.key -out issuer.crt \
+    -subj "/C=$country/ST=$state/L=$locality/O=devang.com/OU=IT Department/CN=devang.com" \
+    -days 36500
+
+# Create certificate
+if [[ "$domain_name" == *"*"* ]]; then
+    folder_name="${domain_name/\*/_}"
+    certificate_name="${domain_name/\*/}"
 else
-  folder_name="${domain_name}"
+    folder_name="$domain_name"
+    certificate_name="$domain_name"
 fi
 
-echo "Enter the country code (2-letter code):"
-read country_code
+if [ ! -d "$folder_name" ]; then
+    mkdir "$folder_name"
+fi
 
-echo "Enter the state/province:"
-read state
+openssl req -new -nodes -newkey rsa:2048 -keyout "$folder_name/$certificate_name.key" -out "$folder_name/$certificate_name.csr" \
+    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizational_unit/CN=$common_name/emailAddress=$email_address"
 
-echo "Enter the locality/city:"
-read city
+openssl x509 -req -in "$folder_name/$certificate_name.csr" -CA issuer.crt -CAkey issuer.key -CAcreateserial -out "$folder_name/$certificate_name.crt" \
+    -days "$expire_days"
 
-echo "Enter the organization:"
-read org
+# Check if certificate is expired and ask for renewal
+if openssl x509 -checkend 0 -noout -in "$folder_name/$certificate_name.crt"; then
+    echo "Certificate is not expired"
+else
+    echo "Certificate has expired or will expire soon"
+    read -p "Do you want to renew the certificate? (Y/N): " renew_certificate
+    if [ "$renew_certificate" == "Y" ]; then
+        read -p "Enter new certificate expire days: " new_expire_days
+        openssl x509 -req -in "$folder_name/$certificate_name.csr" -CA issuer.crt -CAkey issuer.key -CAcreateserial -out "$folder_name/$certificate_name.crt" \
+            -days "$new_expire_days"
+        echo "Certificate renewed"
+    else
+        echo "Certificate not renewed"
+    fi
+fi
 
-echo "Enter the organizational unit:"
-read org_unit
+# Export full chain and PFX
+openssl pkcs12 -export -in "$folder_name/$certificate_name.crt" -inkey "$folder_name/$certificate_name.key" -certfile issuer.crt -out "$folder_name/$certificate_name.pfx" -passout "pass:$passphrase"
 
-echo "Enter the common name (fully qualified domain name):"
-read common_name
+openssl pkcs12 -in "$folder_name/$certificate_name.pfx" -nokeys -out "$folder_name/$certificate_name-fullchain.crt" -passin "pass:$passphrase"
 
-echo "Enter the email address:"
-read email
-
-echo "Enter the SAN (comma separated values, for example: dns:example.com,ip:10.0.0.1):"
-read san
-
-echo "Enter the certificate expiry in days:"
-read expire_days
-
-echo "Enter the passphrase:"
-read -s passphrase
-
-# Create the issuer certificate
-openssl genrsa -out issuerdevang.key 2048
-openssl req -new -x509 -key issuerdevang.key -out issuerdevang.crt -days 36500 -subj "/C=UK/ST=London/L=London/O=devang.com/OU=IT/CN=devang.com/emailAddress=admin@devang.com"
-
-# Create the SSL certificate
-openssl genrsa -out $folder_name/$domain_name.key 2048
-openssl req -new -key $folder_name/$domain_name.key -out $folder_name/$domain_name.csr -subj "/C=$country_code/ST=$state/L=$city/O=$org/OU=$org_unit/CN=$common_name/emailAddress=$email"
-openssl x509 -req -in $folder_name/$domain_name.csr -CA issuerdevang.crt -CAkey issuerdevang.key -CAcreateserial -out $folder_name/$domain_name.crt -days $expire_days -sha256 -extfile <(echo subjectAltName=$san)
-
-# Export the PFX and full chain
-openssl pkcs12 -export -out $folder_name/$domain_name.pfx -inkey $folder_name/$domain_name.key -in $folder_name/$domain_name.crt -certfile issuerdevang.crt -passout pass:$passphrase
-cat $folder_name/$domain_name.crt issuerdevang.crt > $folder_name/full_chain.crt
-
-echo "Certificate for $common_name generated successfully!"
+echo "Full chain and PFX exported successfully"
